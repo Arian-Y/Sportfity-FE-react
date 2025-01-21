507c739359c66c578cbf424ccb50febfec12da1c
Arian-Y <arianyousefi04@gmail.com>
2025-01-21 10:59:36 +0000

    passing over

--- vite-project/src/components/MatchList.jsx
+++ vite-project/src/components/MatchList.jsx
@@ -1,46 +1,46 @@
 import { useEffect, useState } from "react";
 import { getMatches } from "../../api";
 import { Link } from "react-router";
+import MatchCard from "./MatchCard";
 export default function MatchList() {
   const [matches, setMatches] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true);
     getMatches()
       .then((matches) => {
         setMatches(matches);
-        setIsLoading(matches);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.error("Error fetching articles:", err);
         setIsLoading(false);
       });
   }, []);
-}
 
-if (!matches) return <p>No matches here</p>;
+  if (!matches) return <p>No matches here</p>;
 
-return isLoading ? (
-  <section>
-    <p>Loading...</p>
-  </section>
-) : (
-  <>
+  return isLoading ? (
     <section>
-      <p>Here is a list of matches you can join:</p>
-      <ul>
-        {matches.map((match, index) => {
-          return (
-            <section>
-              <ul key={index}>
-                <MatchCard match={match} />
-              </ul>
-            </section>
-          );
-        })}
-      </ul>
-      <button onClick={createMatchLink}>Create Match</button>
+      <p>Loading...</p>
     </section>
-  </>
-);
+  ) : (
+    <>
+      <section>
+        <p>Here is a list of matches you can join:</p>
+        <ul>
+          {matches.map((match, index) => {
+            return (
+              <section>
+                <ul key={index}>
+                  <MatchCard match={match} />
+                </ul>
+              </section>
+            );
+          })}
+        </ul>
+        <button>Create Match</button>
+      </section>
+    </>
+  );
+}
