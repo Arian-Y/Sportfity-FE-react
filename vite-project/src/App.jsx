fbf7dd9fa869d4634d8c20183caed35086f97de1
Ghiffari <ghiffariaulia90@gmail.com>
2025-01-21 20:45:20 +0000

    init CreateMatch component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import League from "./components/League";
 import Footer from "./components/Footer";
 import Match from "./components/Match";
 import "./App.css";
+import CreateMatch from "./components/CreateMatch";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/matches" element={<MatchList />}></Route>
         <Route path="/leagues" element={<League />}></Route>
-        <Route path="/matches/create_match"></Route>
+        <Route path="/matches/create_match" element={<CreateMatch />}></Route>
         <Route path="/matches/:match_id/" element={<Match />}></Route>
       </Routes>
 
---
+++ vite-project/src/components/CreateMatch.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+export default function CreateMatch() {
+  return (
+    <>
+      <p>Create Match...</p>
+    </>
+  );
+}
