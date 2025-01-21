97d51ccc8c0e1afe08ef512b80443f0e2e32271d
“Tayyab <tayyabhaseeb5@gmail.com>
2025-01-21 14:09:21 +0000

    footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import MatchList from "./components/MatchList";
 import Header from "./components/Header";
 
 import { BrowserRouter } from "react-router";
-
-
-
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <MatchList />
+      <Footer />
     </BrowserRouter>
   );
 }
